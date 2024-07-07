1a8d7200a50ac34f87faa8908126500cb6bc83d7
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-07-07 13:26:25 -0300

    MOD -  Módulo de um número

--- exercicios_1.py
+++ exercicios_1.py
@@ -3,3 +3,10 @@ type(1)
 type(2.0)
 type(True)
 type("Tipo String")
+
+# Módulo de um número
+x = 5
+y = 2
+print('resultado da divisão é = ', x/y)
+print('resultado inteiro desta divisão é = ', x//y)
+print('resultado do resto é = ', x%y)
