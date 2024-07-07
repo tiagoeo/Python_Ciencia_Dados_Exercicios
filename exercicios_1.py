39f63a7193a38bcf467fe259b62a2c881ff913ef
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-07-07 13:29:25 -0300

    MOD - Declaração Multipla

--- exercicios_1.py
+++ exercicios_1.py
@@ -10,3 +10,10 @@ y = 2
 print('resultado da divisão é = ', x/y)
 print('resultado inteiro desta divisão é = ', x//y)
 print('resultado do resto é = ', x%y)
+
+# Declaração Multipla
+a = 27//3
+b = a**3
+print('a = ', a)
+print('b', b, sep=' = ')
+print(f"a = {a} e b = {b}")
