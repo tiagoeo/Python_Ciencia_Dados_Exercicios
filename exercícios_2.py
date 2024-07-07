22b83836584c794d41b5a8bb258b93316ea7c1dd
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-07-07 14:22:09 -0300

    MOD - Gerando Intervalos.

--- exercícios_2.py
+++ exercícios_2.py
@@ -6,3 +6,9 @@ lista.sort()
 print("Lista sort() = ", lista)
 lista.reverse()
 print("Lista reverse() = ", lista)
+
+# Gerando Intervalos
+print(range(10))
+print(list(range(0, 10)))
+print(list(range(0,10,2)))
+print(list(range(20,0,-2)))
