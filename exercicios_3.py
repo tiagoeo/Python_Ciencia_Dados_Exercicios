37a9cf3ebba17caa0ca063d5d657ff1d03b14737
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-07-07 14:57:44 -0300

    ADD - exercicios_3.py

---
+++ exercicios_3.py
@@ -0,0 +1,8 @@
+# Operações de conjunto
+# Disjunto
+conj1 = set(range(0, 10, 2))
+conj2 = set(range(1, 11, 2))
+print('Conjunto 1 = ', conj1)
+print('Conjunto 2 = ', conj2)
+conj1.isdisjoint(conj2)
+
