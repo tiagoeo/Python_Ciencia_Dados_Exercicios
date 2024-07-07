1c0f663f1b71396c4c184464827b0817d5450bce
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-07-07 14:31:52 -0300

    ADD - Criando dicionários.

--- exercícios_2.py
+++ exercícios_2.py
@@ -12,3 +12,11 @@ print(range(10))
 print(list(range(0, 10)))
 print(list(range(0,10,2)))
 print(list(range(20,0,-2)))
+
+# Dicionários
+pessoa1 = dict(nome='Victor', idade=17)
+pessoa2 = dict([['nome', 'Ana'],['idade', 26]])
+pessoa3 = {'nome' : 'Paulo', 'idade' : 34}
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
